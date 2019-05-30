eb2c7fa1088f7db1f7000b9c77eb3065a93247b3
DangouHaim <dangouhaim@mail.ru>
2019-05-30 11:08:08 +0300

    Payment fix for unauthorized users

--- wp-content/themes/olam/up_templates/up_sdk/account.php
+++ wp-content/themes/olam/up_templates/up_sdk/account.php
@@ -211,34 +211,110 @@ function getLatestTransaction() {
 }
 
 
-// check payout
-if(isset($_POST["payOut"])) {
-    $value = (float)$_POST["payOut"];
-    $uid = get_current_user_id();
+if(is_user_logged_in()) {
+    // check payout
+    if(isset($_POST["payOut"])) {
+        $value = (float)$_POST["payOut"];
+        $uid = get_current_user_id();
 
-    if($uid && $value && checkSubtractAccount($value)) {
+        if($uid && $value && checkSubtractAccount($value)) {
+            
+            $unitPay = new UnitPay($partnerSecretKey);
+
+            $tid = prepareTransaction((float)$value);
         
+            $response = $unitPay->api('massPayment', [
+            'sum'     => $value,
+            'purse'     => $_POST["purse"],
+            'login'     => $partnerLogin,
+            'transactionId'     => $tid,
+            'paymentType'     => $_POST["type"],
+            ]);
+            
+            if(!isset($response->error->message)) {
+                subtractAccount($value, $uid);
+            } else {
+                $code = $response->error->code;
+                abortTransaction($tid, $code);
+            }
+            
+            // If need user redirect on Payment Gate
+            if (isset($response->result->type)
+            && $response->result->type == 'redirect') {
+            // Url on PaymentGate
+            $redirectUrl = $response->result->redirectUrl;
+            // Payment ID in Unitpay (you can save it)
+            $paymentId = $response->result->paymentId;
+            // User redirect
+            header("Location: " . $redirectUrl);
+            
+            // If without redirect (invoice)
+            } elseif (isset($response->result->type)
+            && $response->result->type == 'invoice') {
+            // Url on receipt page in Unitpay
+            $receiptUrl = $response->result->receiptUrl;
+            // Payment ID in Unitpay (you can save it)
+            $paymentId = $response->result->paymentId;
+            // Invoice Id in Payment Gate (you can save it)
+            $invoiceId = $response->result->invoiceId;
+            // User redirect
+            header("Location: " . $receiptUrl);
+            
+            // If error during api request
+            } elseif (isset($response->error->message)) {
+                $error = $response->error->message;
+                if($_DEBUG) {
+                    print 'Error during api request: '.$error;
+                }
+            }
+        }
+    }
+
+    // add balance request
+    if(isset($_POST["addAccountValue"])) {
+        $value = (float)$_POST["addAccountValue"];
+        if($value) {
+        $orderNumber = prepareOrderNumber();
+        
+        if($orderNumber) {
+            $orderId = $orderNumber;
+            
+            $unitPay = new UnitPay($secretKey);
+    
+            $orderSum = $value;
+    
+            $redirectUrl = $unitPay->form(
+                $publicId,
+                $orderSum,
+                $orderId,
+                $orderDesc,
+                $orderCurrency
+            );
+            
+            header("Location: " . $redirectUrl);
+        }
+        }
+        die;
+    }
+
+    /* UPDATE TRANSACTIONS FOR CURRENT USER */
+    $tid = getLatestTransaction();
+    if($tid) {
         $unitPay = new UnitPay($partnerSecretKey);
 
-        $tid = prepareTransaction((float)$value);
-    
-        $response = $unitPay->api('massPayment', [
-        'sum'     => $value,
-        'purse'     => $_POST["purse"],
-        'login'     => $partnerLogin,
-        'transactionId'     => $tid,
-        'paymentType'     => $_POST["type"],
+        $response = $unitPay->api('massPaymentStatus', [
+            'login'     => $partnerLogin,
+            'transactionId'     => $tid,
         ]);
-        
-        if(!isset($response->error->message)) {
-            subtractAccount($value, $uid);
-        } else {
-            $code = $response->error->code;
-            abortTransaction($tid, $code);
-        }
-        
-        // If need user redirect on Payment Gate
-        if (isset($response->result->type)
+
+        // Update transaction with subtracting money from user account
+        if(isset($response->result->status)) {
+            $result = $response->result;
+
+            if($result->status == "success") {
+                updateTransaction($tid);
+            }
+        } elseif (isset($response->result->type)
         && $response->result->type == 'redirect') {
         // Url on PaymentGate
         $redirectUrl = $response->result->redirectUrl;
@@ -246,7 +322,7 @@ if(isset($_POST["payOut"])) {
         $paymentId = $response->result->paymentId;
         // User redirect
         header("Location: " . $redirectUrl);
-        
+
         // If without redirect (invoice)
         } elseif (isset($response->result->type)
         && $response->result->type == 'invoice') {
@@ -258,95 +334,21 @@ if(isset($_POST["payOut"])) {
         $invoiceId = $response->result->invoiceId;
         // User redirect
         header("Location: " . $receiptUrl);
-        
+
         // If error during api request
         } elseif (isset($response->error->message)) {
             $error = $response->error->message;
+            $code = $response->error->code;
+
+            abortTransaction($tid, $code);
+
             if($_DEBUG) {
-                print 'Error during api request: '.$error;
+                print 'Error during update transactions(' . $tid . '): '.$error;
             }
         }
     }
+    /* UPDATE TRANSACTIONS FOR CURRENT USER END */
 }
-
-// add balance request
-if(isset($_POST["addAccountValue"])) {
-    $value = (float)$_POST["addAccountValue"];
-    if($value) {
-      $orderNumber = prepareOrderNumber();
-      
-      if($orderNumber) {
-        $orderId = $orderNumber;
-        
-        $unitPay = new UnitPay($secretKey);
-  
-        $orderSum = $value;
-  
-        $redirectUrl = $unitPay->form(
-            $publicId,
-            $orderSum,
-            $orderId,
-            $orderDesc,
-            $orderCurrency
-        );
-        
-        header("Location: " . $redirectUrl);
-      }
-    }
-    die;
-  }
-
-/* UPDATE TRANSACTIONS FOR CURRENT USER */
-$tid = getLatestTransaction();
-if($tid) {
-    $unitPay = new UnitPay($partnerSecretKey);
-
-    $response = $unitPay->api('massPaymentStatus', [
-        'login'     => $partnerLogin,
-        'transactionId'     => $tid,
-    ]);
-
-    // Update transaction with subtracting money from user account
-    if(isset($response->result->status)) {
-        $result = $response->result;
-
-        if($result->status == "success") {
-            updateTransaction($tid);
-        }
-    } elseif (isset($response->result->type)
-    && $response->result->type == 'redirect') {
-    // Url on PaymentGate
-    $redirectUrl = $response->result->redirectUrl;
-    // Payment ID in Unitpay (you can save it)
-    $paymentId = $response->result->paymentId;
-    // User redirect
-    header("Location: " . $redirectUrl);
-
-    // If without redirect (invoice)
-    } elseif (isset($response->result->type)
-    && $response->result->type == 'invoice') {
-    // Url on receipt page in Unitpay
-    $receiptUrl = $response->result->receiptUrl;
-    // Payment ID in Unitpay (you can save it)
-    $paymentId = $response->result->paymentId;
-    // Invoice Id in Payment Gate (you can save it)
-    $invoiceId = $response->result->invoiceId;
-    // User redirect
-    header("Location: " . $receiptUrl);
-
-    // If error during api request
-    } elseif (isset($response->error->message)) {
-        $error = $response->error->message;
-        $code = $response->error->code;
-
-        abortTransaction($tid, $code);
-
-        if($_DEBUG) {
-            print 'Error during update transactions(' . $tid . '): '.$error;
-        }
-    }
-}
-/* UPDATE TRANSACTIONS FOR CURRENT USER END */
 
 /* PAYMENT HANDLER */
 if(isset($_GET["payhandler"])) {
