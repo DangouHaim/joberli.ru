aa84cbdd3afa1c2e10570dcdd429c23b3cde8652
DangouHaim <dangouhaim@mail.ru>
2019-06-20 15:31:57 +0300

    Added tab for completed orders

--- wp-content/themes/olam/fes_templates/frontend-products.php
+++ wp-content/themes/olam/fes_templates/frontend-products.php
@@ -6,6 +6,7 @@
     <li><a href="#fragment-1">Мои товары</a></li>
 	<li><a href="#fragment-2">Мои покупки</a></li>
 	<li><a href="#fragment-3">Мои задачи</a></li>
+	<li><a href="#fragment-4">Завершённые</a></li>
   </ul>
 
   <div id="fragment-1">
@@ -95,8 +96,17 @@
 
 			if (count($orders) > 0 ){
 				foreach ( $orders as $product ) : ?>
+				<?php
+					$orderId = $product->purchase->id;
+					
+					if(getOrderStatus($orderId) == "Выполнен"
+						|| getOrderStatus($orderId) == "Отменён") {
+						continue;
+					}
+				?>
 				<tr>
 					<td class = "fes-product-list-td"><?php
+
 					$featImage=null;
 					$theDownloadImage=get_post_meta($product->ID,'download_item_thumbnail_id'); 
 					if(is_array($theDownloadImage) && (count($theDownloadImage)>0) ){
@@ -109,8 +119,6 @@
 						$featImage=wp_get_attachment_image_src($thumbID,'olam-product-thumb-small');
 						$featImage=$featImage[0];
 					}
-
-					$orderId = $product->purchase->id;
 
 					?>
 					<?php if(isset($featImage)&&strlen($featImage)>0) { 
@@ -136,6 +144,8 @@
 						<?php if( !isCancelledOrder($orderId) && !isOrderDone($orderId) && isOrderHasDoneRequest($orderId) ): ?>
 							<a href="#" class="tabs-button fa fa-check-square-o confirm-order-done" data-order-id="<?php echo $orderId; ?>" data-discription="Подтвердить выполнение"></a>
 						<?php endif; ?>
+
+						<a href="http://joberli.ru/messages/?user=<? echo getOrderPostOwner($orderId)?>&tab=chat" class="tabs-button fa fa-comment-o" data-order-id="<?php echo $orderId; ?>" data-discription="Связаться с исполнителем"></a>
 						
 					</td>
 
@@ -179,8 +189,17 @@
 
 			if (count($orders) > 0 ){
 				foreach ( $orders as $product ) : ?>
+				<?php
+					$orderId = $product->purchase->id;
+					
+					if(getOrderStatus($orderId) == "Выполнен"
+						|| getOrderStatus($orderId) == "Отменён") {
+						continue;
+					}
+				?>
 				<tr>
 					<td class = "fes-product-list-td"><?php
+
 					$featImage=null;
 					$theDownloadImage=get_post_meta($product->ID,'download_item_thumbnail_id'); 
 					if(is_array($theDownloadImage) && (count($theDownloadImage)>0) ){
@@ -193,8 +212,6 @@
 						$featImage=wp_get_attachment_image_src($thumbID,'olam-product-thumb-small');
 						$featImage=$featImage[0];
 					}
-
-					$orderId = $product->purchase->id;
 
 					?>
 					<?php if(isset($featImage)&&strlen($featImage)>0) { 
@@ -226,6 +243,94 @@
 						<?php endif; ?>
 
 						<a href="http://joberli.ru/messages/?user=<? echo getUser($orderId)?>&tab=chat" class="tabs-button fa fa-comment-o" data-order-id="<?php echo $orderId; ?>" data-discription="Связаться с клиентом"></a>
+
+					</td>
+
+					<td class = "fes-product-list-td"><?php echo EDD_FES()->dashboard->product_list_date($product->ID); ?></td>
+					<?php do_action('fes-product-table-column-value'); ?>
+				</tr>
+				<?php endforeach;
+			} else {
+				echo '<tr><td colspan="7" class = "fes-product-list-td" >'. sprintf( _x('No %s found', 'FES lowercase plural setting for download','edd_fes'), EDD_FES()->helper->get_product_constant_name( $plural = true, $uppercase = false ) ).'</td></tr>';
+			}
+			?>
+		</tbody>
+	</table>
+  </div>
+
+  <div id="fragment-4">
+  	<table class="table fes-table table-condensed  table-striped" id="fes-product-list">
+		<thead>
+			<tr>
+				<th><?php _e( 'Картинка', 'edd_fes' ); ?></th>
+				<th><?php _e( '№', 'edd_fes' ); ?></th>
+				<th><?php _e( 'Имя', 'edd_fes' ); ?></th>
+				<th><?php _e( 'Статус', 'edd_fes' ); ?></th>
+				<th><?php _e( 'Цена', 'edd_fes' ); ?></th>
+				<th><?php _e( 'Действия','edd_fes') ?></th>
+				<th><?php _e( 'Дата', 'edd_fes' ); ?></th>
+				<?php do_action('fes-product-table-column-title'); ?>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			$orders = array();
+
+			$purchases = getUserOrders();
+
+			foreach ( $purchases as $purchase ) {
+				$download = edd_get_download($purchase->postId);
+				$download->purchase = $purchase;
+				array_push($orders, $download);
+			}
+
+			if (count($orders) > 0 ){
+				foreach ( $orders as $product ) : ?>
+				<?php
+					$orderId = $product->purchase->id;
+					
+					if(getOrderStatus($orderId) != "Выполнен"
+						&& getOrderStatus($orderId) != "Отменён") {
+						continue;
+					}
+				?>
+				<tr>
+					<td class = "fes-product-list-td"><?php
+
+					$featImage=null;
+					$theDownloadImage=get_post_meta($product->ID,'download_item_thumbnail_id'); 
+					if(is_array($theDownloadImage) && (count($theDownloadImage)>0) ){
+						$thumbID=$theDownloadImage[0];
+						$featImage=wp_get_attachment_image_src($thumbID,'olam-product-thumb-small');
+						$featImage=$featImage[0];
+					}
+					else{
+						$thumbID=get_post_thumbnail_id($product->ID);
+						$featImage=wp_get_attachment_image_src($thumbID,'olam-product-thumb-small');
+						$featImage=$featImage[0];
+					}
+
+					?>
+					<?php if(isset($featImage)&&strlen($featImage)>0) { 
+						$alt = get_post_meta($thumbID, '_wp_attachment_image_alt', true);
+						?> 
+						<img src="<?php echo esc_url($featImage);  ?>" class="attachment-shop_thumbnail wp-post-image" alt="<?php echo esc_attr($alt); ?>">
+						<?php } ?>
+					</td>
+					<td class = "fes-product-list-td"><?php echo $orderId; ?></td>
+					<td class = "fes-product-list-td"><?php echo EDD_FES()->dashboard->product_list_title($product->ID); ?></td>
+					<td class = "fes-product-list-td"><?php echo getOrderStatus($orderId); ?></td>
+					<td class = "fes-product-list-td"><?php echo $product->purchase->sum; ?></td>
+					
+					<td class = "fes-product-list-td">
+						
+						<?php EDD_FES()->dashboard->product_list_actions($product->ID); ?>
+
+						<? if(isOrderPostOwner($orderId)) : ?>
+							<a href="http://joberli.ru/messages/?user=<? echo getOrderPostOwner($orderId)?>&tab=chat" class="tabs-button fa fa-comment-o" data-order-id="<?php echo $orderId; ?>" data-discription="Связаться с исполнителем"></a>
+						<? else :?>
+							<a href="http://joberli.ru/messages/?user=<? echo getUser($orderId)?>&tab=chat" class="tabs-button fa fa-comment-o" data-order-id="<?php echo $orderId; ?>" data-discription="Связаться с клиентом"></a>
+						<?endif?>
 
 					</td>
 
