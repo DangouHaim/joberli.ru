fd3994ff22347db85b4d75b4d389978016e1aabc
DangouHaim <dangouhaim@mail.ru>
2019-08-02 16:27:16 +0300

    Return logo

--- wp-content/themes/olam/functions.php
+++ wp-content/themes/olam/functions.php
@@ -2494,7 +2494,7 @@ function convertYoutube($string) {
 function getYoutubeEmbedUrl($url)
 {
     $shortUrlRegex = '/youtu.be\/([a-zA-Z0-9_-]+)\??/i';
-	$longUrlRegex = '/youtube.com\/((?:embed)|(?:watch))((?:\?v\=)|(?:\/))([a-zA-Z0-9_-]+)/i';
+	  $longUrlRegex = '/youtube.com\/((?:embed)|(?:watch))((?:\?v\=)|(?:\/))([a-zA-Z0-9_-]+)/i';
 
     if (preg_match($longUrlRegex, $url, $matches)) {
         $youtube_id = $matches[count($matches) - 1];
